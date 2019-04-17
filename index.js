16fae0fcc95cf9931d0db6873e7c87742df58c8b
Xinran Che <gracexinran@gmail.com>
2019-04-17 15:29:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 function takeANumber(katzDeliLine){
-
-  katzDeliLine.push(katzDeliLine.length);
-    return `Welcome you are number ${katzDeliLine.length}.`;
+  let counter = 0;
+  counter += 1;
+  katzDeliLine.push(counter);
+    return `Welcome you are number ${counter}.`;
     // Welcome you are number 28
 }
+const array = []
+takeANumber(array) // =》 Welcome you are number 1
+takeANumber(array) // =》 Welcome you are number 2
+takeANumber(array) // =》 Welcome you are number 3
+nowServing(array) // => Currently serving 1
+takeANumber(array) // => Welcome you are number 3
+
+// [2,3,3]
 
 function nowServing(katzDeliLine){
     if(katzDeliLine.length){
